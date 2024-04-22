4b40491ed3f0b71d4a50973a9e92b7349b538e1f
Yashanshh <yashansh726@gmail.com>
2024-04-22 18:18:45 +0530

    Save data and generate HTML for payment summary

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,3 +1,16 @@
+export function getProducts(productId) {
+  let matchingProducts;
+
+  products.forEach((product) => {
+    if(product.id === productId) {
+      matchingProducts = product;
+    }
+  });
+
+  return matchingProducts;
+}
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
