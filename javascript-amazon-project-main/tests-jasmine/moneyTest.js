d1c3d4a3265915c734d37db6f459569ca5bc7fbd
Yashanshh <yashansh726@gmail.com>
2024-04-25 20:55:16 +0530

    Remove default tests and add money tests

---
+++ javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+describe('Test Suite: formatCurrency', () => {
+  it('Convert cents into dollars', ()=> {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('Works with 0', ()=> {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('Rounds up to the nearest cent', ()=> {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+  
+});
