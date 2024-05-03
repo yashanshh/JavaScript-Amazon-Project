d8a8a71362a3dda1c5655fc57c0bd25d43359219
Yashanshh <yashansh726@gmail.com>
2024-05-03 15:17:29 +0530

    Load products from the backend

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -79,7 +79,30 @@ class Clothing extends Product {
 // };
 // object3.method();
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -745,3 +768,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
